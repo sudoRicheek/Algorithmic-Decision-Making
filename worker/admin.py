bb3e46acd8cc6d087693808bf04a5fe0f38c7b7b
Richeek Das <60005701+sudoRicheek@users.noreply.github.com>
2021-07-11 20:17:07 +0530

    beautify admin

--- worker/admin.py
+++ worker/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 
 from .models import Worker
 
+admin.site.site_header = 'ADM HIT - Dashboard'
+
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ()
+    list_filter = ()
+    
+
 admin.site.register(Worker)
+admin.site.unregister([User, Group])
