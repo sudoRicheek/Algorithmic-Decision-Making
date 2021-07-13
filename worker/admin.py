eca03e866cd70157c1860701e9a0d337dd63b7e4
Richeek Das <60005701+sudoRicheek@users.noreply.github.com>
2021-07-13 20:17:03 +0530

    admin enhanced

--- worker/admin.py
+++ worker/admin.py
@@ -6,9 +6,13 @@ from .models import Worker
 admin.site.site_header = 'ADM HIT - Dashboard'
 
 class WorkerAdmin(admin.ModelAdmin):
-    list_display = ()
-    list_filter = ()
-    
+    list_display = ('worker_id', 
+            'attention_passed', 
+            'comprehension_passed', 
+            'type_work', 
+            'belief_elicitation_attempted', 
+            'postexperimental_submitted')
+    list_filter = ('attention_passed','comprehension_passed')
 
-admin.site.register(Worker)
+admin.site.register(Worker, WorkerAdmin)
 admin.site.unregister([User, Group])
